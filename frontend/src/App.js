264dc6250c5f0a8ffae6961d1e5aaf75769e9607
Dooneyy <delzeen55@gmail.com>
2021-12-02 11:25:46 +0100

    Builidng components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,34 @@
-import * as React from 'react';
+import React from 'react';
+import Filter from './components/Filter';
+import Cart from './components/Cart';
+import Market from './components/Market';
 
 function App() {
+
   return (
-    <div className="p-20 bg-black text-white text-center">
-      <p className='text-3xl'>Robot Market</p>
-      
+    <div className="p-6">
+      <div className="grid grid-cols-12 grid-rows-auto gap-6 my-8">
+
+		<div className="col-span-2">
+			<div className="fixed">
+				<div className="mb-8">
+					<p className='text-3xl'>Robot Market</p>
+				</div>
+			<Filter />
+			</div>
+		</div>
+
+		<div className="col-span-8">
+			<Market />
+		</div>
+
+		<div className="col-span-2">
+			<div className="fixed">
+				<Cart />
+			</div>
+		</div>
+	  	
+	  </div>
     </div>
   );
 }
---
+++ frontend/src/components/Market.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Market = () => {
+    return (
+        <div className='grid grid-cols-12 grid-rows-auto gap-7'>
+            <div className="col-span-4 h-48 bg-red-200">Hello</div>
+            <div className="col-span-4 h-48 bg-gray-200">Hello</div>
+            <div className="col-span-4 h-48 bg-blue-200">hello</div>
+            <div className="col-span-4 h-48 bg-green-300">hello</div>
+            <div className="col-span-4 h-48 bg-pink-300">hello</div>
+            <div className="col-span-4 h-48 bg-red-700">hello</div>
+            <div className="col-span-4 h-48 bg-gray-900">hello</div>
+            <div className="col-span-4 h-48 bg-green-900">hello</div>
+
+            
+        </div>
+    )
+}
+
+export default Market
