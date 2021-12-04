b2548e2f53458b3727f32f6da0b53f044c8757fc
Dooneyy <delzeen55@gmail.com>
2021-12-04 20:35:06 +0100

    robot-market

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,35 +1,86 @@
-import React from 'react';
-import Filter from './components/Filter';
-import Cart from './components/Cart';
+import React, {  useState, useEffect } from 'react';
+import Loader from "react-loader-spinner";
 import Market from './components/Market';
+import { AiOutlineShoppingCart } from "react-icons/ai";
 
-function App() {
+const url = 'http://localhost:8000/api/robots';
+
+const App = () => {
+  const [robots, setRobots] = useState([]);
+  const [loading, setLoading] = useState(true)
+ 
+  const arr1 = [];
+  robots.map((item) => 
+    arr1.push(item.material)
+  )
+  const uniqueMaterials = [...new Set(arr1)];
+  console.log(uniqueMaterials);
+
+  const fetchRobot = async () => {
+    setLoading(true);
+        try {
+            const response = await fetch (url);
+            const robots = await response.json();
+            console.log(robots);
+            setLoading(false);
+            setRobots(robots.data);
+        } catch (error) {
+            setLoading(false);
+            console.log(error);
+        }
+    }
+
+    useEffect(() => {
+       fetchRobot();
+    }, [])
+
+   if(loading) {
+    return <main className='w-1/5 my-36 mx-auto text-center'>
+      <Loader type="Hearts" color="#00BFFF" height={150} width={150} />
+    </main>
+  }
 
   return (
-    <div className="p-6">
-      <div className="grid grid-cols-12 grid-rows-auto gap-6 my-8">
+    <main className="p-4">
+    	<p className='text-5xl font-bold text-gray-400 text-center'>ROBOT MARKET</p>
+      
+    <div className="grid grid-cols-12 grid-rows-auto gap-6 my-8 text-gray-700 relative">
 
-		<div className="col-span-2">
-			<div className="fixed">
-				<div className="mb-8">
-					<p className='text-3xl'>Robot Market</p>
-				</div>
-			<Filter />
-			</div>
+		<div className="col-span-3 fixed top-8 w-1/4">
+          <p className='my-6 text-2xl font-bold'>Product Material</p>
+          <div className="flex justify-around items-center flex-wrap mr-6">
+            { 
+              uniqueMaterials.map(material => {
+                  return (
+                     <button className='font-semiBold material text-white m-2'>{material}</button> 
+                  
+                 
+                  )
+              })
+          }
+        </div>
+        <div className="mt-28">
+            <button className='flex justify-center items-center text-center p-6 bg-red-500 hover:bg-red-700 rounded w-2/4 h-12 text-white'>
+                    <AiOutlineShoppingCart className='text-3xl mr-4' /> Cart <sup className='ml-3 bg-white text-black p-2'>2</sup>
+            </button>
+        </div>
 		</div>
 
-		<div className="col-span-8">
-			<Market />
+		<div className="col-span-9 absolute right-0 w-3/4">
+			 <div className='grid grid-cols-12 grid-rows-auto gap-6'>
+        {
+            robots.map(item => {
+                return (
+                  <Market name={item.name} stock={item.stock} image={item.image} createdAt={item.createdAt} material={item.material} price={item.price} />
+                )
+            })
+        }
+           
+        </div>
 		</div>
 
-		<div className="col-span-2">
-			<div className="fixed">
-				<Cart />
-			</div>
-		</div>
-	  	
 	  </div>
-    </div>
+    </main>
   );
 }
 
--- frontend/src/components/Market.js
+++ frontend/src/components/Market.js
@@ -1,19 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import Moment from 'react-moment';
 
-const Market = () => {
+const Market = ({ name, stock, image, createdAt, material, price }) => {
+    
     return (
-        <div className='grid grid-cols-12 grid-rows-auto gap-7'>
-            <div className="col-span-4 h-48 bg-red-200">Hello</div>
-            <div className="col-span-4 h-48 bg-gray-200">Hello</div>
-            <div className="col-span-4 h-48 bg-blue-200">hello</div>
-            <div className="col-span-4 h-48 bg-green-300">hello</div>
-            <div className="col-span-4 h-48 bg-pink-300">hello</div>
-            <div className="col-span-4 h-48 bg-red-700">hello</div>
-            <div className="col-span-4 h-48 bg-gray-900">hello</div>
-            <div className="col-span-4 h-48 bg-green-900">hello</div>
+        <div className="col-span-4 bg-gray-600 text-white relative robot-card">
+                    <div className='absolute top-0 right-0 font-bold text-lg bg-red-500 text-white px-4 py-3 stock'>
+                        <p className='text-center'>{stock === 0 ? 'Out Of Stock' : `Stock left : ${stock}`}</p>
+                    </div>
+                    
+                <div className="p-6 ">
+                <div className="image">
+                    <img src={image} alt={name}/>
+                </div>
 
-            
-        </div>
+                <div className="text-center my-8">
+                <div className="flex justify-between items-center my-4">
+                    <Moment format="DD-MM-YYYY" date={createdAt} className='font-bold date'/>
+                    <p className='font-semiBold material'> {material}</p>
+                </div>
+
+                    <p className='text-2xl font-Bold mb-2'>{name}</p>
+                    <p className='text-yellow-400 text-xl font-bold mb-2'>฿ {price}</p>
+                </div>
+
+                {
+                    stock === 0 ? '' :  
+                    <button className='justify-self-center flex justify-center items-center text-center p-6 bg-red-500 hover:bg-red-700 rounded w-3/4 h-12'>
+                        <AiOutlineShoppingCart className='text-2xl mr-4' /> Add to cart
+                    </button>
+                }
+
+                </div> 
+                </div>
+       
     )
 }
 
